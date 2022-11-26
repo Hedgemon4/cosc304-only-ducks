e692f4f9048f4be871bda7c7fa99a39e0a5665bb
te-sa <teresa_saller@freenet.de>
2022-11-25 20:41:05 -0800

    fixed the issue?

--- routes/ship.js
+++ routes/ship.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sql = require('mssql');
 const moment = require('moment');
 
-router.get('/', async function (req, res, next) {
+router.get('/', async function (req, res) {
     res.setHeader('Content-Type', 'text/html');
 
     let orderId = "";
@@ -38,16 +38,16 @@ router.get('/', async function (req, res, next) {
                 let pool = await sql.connect(dbConfig);
                 const transaction = new sql.Transaction(pool);
                 try {
-                    // TODO: Start a transaction
+                    // starting transaction
                     await transaction.begin();
 
-                    // TODO: Retrieve all items in order with given id
+                    // retrieving all items in order with given id
                     let getOrderProducts = "SELECT orderproduct.productId, orderproduct.quantity FROM orderproduct WHERE orderproduct.orderId = @orderId;"
                     let results = await pool.request().input('orderId', sql.Int(), orderId).query(getOrderProducts);
                     let orderProducts = results.recordset;
                     console.log(orderProducts);
 
-                    // TODO: Create a new shipment record.
+                    // creating new shipment record
                     let createShipment = "INSERT INTO shipment (shipmentDate, warehouseId) VALUES (@todaysDate, 1);";
                     await pool.request().input('todaysDate', sql.DateTime, moment().format('Y-MM-DD HH:mm:ss')).query(createShipment);
 
@@ -55,27 +55,32 @@ router.get('/', async function (req, res, next) {
                     let shipments = toBePrinted.recordset;
                     console.log(shipments);
 
-                    // TODO: For each item verify sufficient quantity available in warehouse 1.
+                    // Verifying that there is sufficient quantity available in warehouse 1 for each item
                     // TODO: If any item does not have sufficient inventory, cancel transaction and rollback. Otherwise, update inventory for each item.
                     let getQuantityInWarehouse = "SELECT productinventory.quantity FROM productinventory WHERE productinventory.productId = @productId AND productinventory.warehouseId = 1";
                     let updateInventory = "UPDATE productinventory SET productinventory.quantity = @newQty WHERE productinventory.productId = @productId AND productinventory.warehouseId = 1"
                     for (let i = 0; i < orderProducts.length; i++) {
-                        let orderProduct = orderProducts[i].quantity;
+                        let orderProduct = orderProducts[i];
                         if (!orderProduct) {
                             continue
                         }
+                        console.log("OP: " + orderProduct);
+
                         let results2 = await pool.request().input('productId', sql.Int(), orderProduct.productId).query(getQuantityInWarehouse);
                         let quantityInWarehouse = 0;
                         if (results2.recordset.length !== 0) {
                             quantityInWarehouse = results2.recordset[0].quantity;
                         }
-                        res.write("Quantity in warehouse: "+quantityInWarehouse);
-                        res.write("Quantity ordered: "+orderProduct.quantity);
+
+                        res.write("Quantity in warehouse: " + quantityInWarehouse);
+                        res.write("Quantity ordered: " + orderProduct.quantity);
+
                         if (quantityInWarehouse < orderProduct.quantity) {
                             res.write("Not enough stock!");
                             await transaction.rollback();
                             res.end();
                         }
+
                         let newQty = quantityInWarehouse - orderProduct.quantity;
                         await pool.request().input('newQty', sql.Int(), newQty).input('productId', sql.Int(), orderProduct.productId).query(updateInventory);
                     }
