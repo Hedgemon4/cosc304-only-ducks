53870d546c95cc316dfd2f4795c3dd4d0f466ef7
te-sa <teresa_saller@freenet.de>
2022-11-25 16:23:34 -0800

    on the right track?

--- routes/ship.js
+++ routes/ship.js
@@ -22,6 +22,7 @@ router.get('/', async function (req, res, next) {
             let pool = await sql.connect(dbConfig);
             let sqlQuery = "SELECT * FROM ordersummary WHERE orderId = @orderId;";
             let result = await pool.request().input('orderId', sql.Int(), orderId).query(sqlQuery);
+            pool.close();
             return result.recordset.length !== 0;
         } catch (err) {
             console.dir(err);
@@ -37,22 +38,32 @@ router.get('/', async function (req, res, next) {
     }
 
     await (async function () {
-        if (isPositiveInteger(customerId) && await orderIdInDatabase()) {
+        if (isPositiveInteger(orderId) && await orderIdInDatabase()) {
             try {
                 let pool = await sql.connect(dbConfig);
+                let todaysDate = (new Date()).toISOString();
                 const transaction = new sql.Transaction(pool);
                 try {
                     // TODO: Start a transaction
                     await transaction.begin();
 
-                    const request = new sql.Request(transaction);
+                    // TODO: Retrieve all items in order with given id
+                    let getOrderProducts = "SELECT orderproduct.productId, orderproduct.quantity FROM orderproduct WHERE orderproduct.orderId = @orderId;\""
+                    let results = await pool.request().input('orderId', sql.Int(), orderId).query(getOrderProducts);
+                    let products = results.recordset;
+                    console.log(products);
 
-                    const results = await Promise.all([
-                        // TODO: Retrieve all items in order with given id
-                        // TODO: Create a new shipment record.
-                        // TODO: For each item verify sufficient quantity available in warehouse 1.
-                        // TODO: If any item does not have sufficient inventory, cancel transaction and rollback. Otherwise, update inventory for each item.
-                    ]);
+                    // TODO: Create a new shipment record.
+                    let createShipment = "INSERT INTO shipment (shipmentDate, warehouseId) VALUES ('@todaysDate', 1);";
+                    await pool.request().input('todaysDate', sql.Date(), todaysDate).query(createShipment);
+
+                    let results2 = await pool.request().query("SELECT * FROM shipment;");
+                    let shipments = results2.recordset;
+                    console.log(shipments);
+
+                    // TODO: For each item verify sufficient quantity available in warehouse 1.
+                    // "SELECT * FROM productinventory;"
+                    // TODO: If any item does not have sufficient inventory, cancel transaction and rollback. Otherwise, update inventory for each item.
 
                     await transaction.commit();
                 } catch (err) {
@@ -67,9 +78,10 @@ router.get('/', async function (req, res, next) {
                 res.end();
             }
         }
-        res.write("Hello");
-        res.end();
     })();
+
+    res.write("Hello");
+    res.end();
 });
 
 module.exports = router;
