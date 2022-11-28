808d5c4f75d4756749a8d22c2947e31afd851332
liamdavidson <ldbdavidson@gmail.com>
2022-11-27 19:34:29 -0800

    Added functionality for second image if one is provided

--- routes/product.js
+++ routes/product.js
@@ -11,20 +11,12 @@ router.get('/', function (req, res, next) {
         try {
             let pool = await sql.connect(dbConfig);
 
-            // Get product name to search for
-            // TODO: Retrieve and display info for the product
             const ps = new sql.PreparedStatement(pool)
             ps.input('param', sql.VarChar(40))
-            await ps.prepare("SELECT product.productId, product.productName, product.productPrice, product.productDesc, productImageURL  FROM product WHERE product.productId = @param")
+            await ps.prepare("SELECT product.productId, product.productName, product.productPrice, product.productDesc, productImageURL, productImage  FROM product WHERE product.productId = @param")
             let result = await ps.execute({param: id})
 
             let product = result.recordset
-
-            // TODO: If there is a productImageURL, display using IMG tag
-
-            // TODO: Retrieve any image stored directly in database. Note: Call displayImage.jsp with product id as parameter.
-
-            // TODO: Add links to Add to Cart and Continue Shopping
 
             res.render('productDetails', {id: id, product: product, title: "OnlyDucks Products"})
         } catch (err) {
