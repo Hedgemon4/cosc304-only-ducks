a31902fc5bec5337c9ad07947936b235d746a480
te-sa <teresa_saller@freenet.de>
2022-12-04 12:22:35 -0800

    added filtering by category

--- routes/listprod.js
+++ routes/listprod.js
@@ -6,16 +6,31 @@ router.get('/', function (req, res) {
     let name = "";
     if (req.query.productName) name = req.query.productName;
 
+    let category = "";
+    if (req.query.categoryName) category = req.query.categoryName;
+
     (async function () {
         let pool = false;
         try {
             pool = await sql.connect(dbConfig)
 
+            console.log("\t\t\t" + category);
+
             const ps = new sql.PreparedStatement(pool)
             ps.input('param', sql.VarChar(40))
-            await ps.prepare("SELECT product.productId, product.productName, product.productPrice, product.productDesc, productImageURL FROM product WHERE product.productName LIKE '%' + @param + '%'")
 
-            let results = await ps.execute({param: name})
+            const ps1 = new sql.PreparedStatement(pool)
+            ps1.input('param', sql.VarChar(40))
+            ps1.input('param2', sql.VarChar(40))
+
+            let results
+            if (category === 'All') {
+                await ps.prepare("SELECT product.productId, product.productName, product.productPrice, product.productDesc, productImageURL FROM product WHERE product.productName LIKE '%' + @param + '%'")
+                results = await ps.execute({param: name})
+            } else {
+                await ps1.prepare("SELECT product.productId, product.productName, product.productPrice, product.productDesc, productImageURL FROM product WHERE product.productName LIKE '%' + @param + '%' AND product.categoryId = (SELECT category.categoryId FROM category WHERE category.categoryName = @param2);")
+                results = await ps1.execute({param: name, param2: category})
+            }
 
             let product = results.recordset
 
