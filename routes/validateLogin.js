5c1dbdc8a10836adf8e9412c5942686321583ad4
Hedgemon4 <sethlinc4@gmail.com>
2022-12-06 11:27:46 -0800

    Initial commit

--- routes/validateLogin.js
+++ routes/validateLogin.js
@@ -13,14 +13,17 @@ router.post('/', function (req, res) {
             req.session.authenticatedUser = authenticatedUser
             if (req.session.loginRedirect) {
                 req.session.loginRedirect = false
+                await loadCart(req, res)
                 res.redirect("/checkout");
-            } else
+            } else {
+                await loadCart(req, res)
                 res.redirect("/");
+            }
         } else {
             res.redirect("/login")
         }
     })();
-});
+})
 
 async function validateLogin(req) {
     if (!req.body || !req.body.username || !req.body.password) {
@@ -61,6 +64,103 @@ async function validateLogin(req) {
     })();
 
     return authenticatedUser
+}
+
+async function loadCart(req, res) {
+    let pool = false
+    try {
+        pool = await sql.connect(dbConfig)
+
+        const ps = new sql.PreparedStatement(pool)
+        let customerId = req.session.customerId
+        ps.input('customerId', sql.Int)
+        await ps.prepare('SELECT incart.orderId, incart.productId, incart.quantity, incart.price, product.productName FROM incart JOIN ordersummary ON incart.orderId = ordersummary.orderId JOIN product ON incart.productId = product.productId WHERE ordersummary.customerId = @customerId')
+        let results = await ps.execute({customerId: customerId})
+        let cart = results.recordset
+
+        let orderId = false
+        let products = []
+
+        // Get or create productList
+        let productList = false;
+        if (!req.session.productList) {
+            productList = []
+        } else {
+            productList = req.session.productList
+        }
+        if (cart[0]) {
+            // If incart has values
+            orderId = cart[0].incart.orderId
+            req.session.orderId = orderId
+            for (let i = 0; i < cart.length; i++) {
+                // want to add inCart values to the session, and update the database if the cart also had some values
+                let id = cart[i].incart.productId
+                let name = cart[i].product.productName
+                let price = cart[i].incart.price
+                let quantity = cart[i].incart.quantity
+                products.push(id)
+                if (productList[id]) {
+                    quantity += productList[id].quantity
+                    productList[id].quantity = quantity
+                    const updateInCart = new sql.PreparedStatement(pool)
+                    updateInCart.input('orderId', sql.Int)
+                    updateInCart.input('productId', sql.Int)
+                    updateInCart.input('quantity', sql.Int)
+                    await updateInCart.prepare('UPDATE incart SET quantity = @quantity WHERE orderId = @orderId AND productId = @productId')
+                    await updateInCart.execute({orderId: orderId, productId: id, quanity: quantity})
+                } else {
+                    productList[id] = {
+                        "id": id,
+                        "name": name,
+                        "price": price,
+                        "quantity": quantity
+                    };
+                }
+            }
+        } else {
+            // if incart does not have values
+
+            // make a new order
+            const ps2 = new sql.PreparedStatement(pool)
+            ps2.input('customerId', sql.Int)
+            ps2.input('date', sql.DateTime)
+            await ps2.prepare('INSERT INTO ordersummary (customerId, orderDate, totalAmount) OUTPUT INSERTED.orderId VALUES (@customerId, @date, 0)')
+            let results = await ps2.execute({customerId: customerId, date: new Date()})
+            orderId = results.recordset[0].orderId
+            req.session.orderId = orderId
+        }
+
+        // Then, we need to insert any items in the session but not in the database into the database
+
+        const insertInCart = new sql.PreparedStatement(pool)
+        insertInCart.input('orderId', sql.Int)
+        insertInCart.input('productId', sql.Int)
+        insertInCart.input('quantity', sql.Int)
+        insertInCart.input('price', sql.Decimal(10, 2))
+        await insertInCart.prepare('INSERT INTO incart (orderId, productId, quantity, price) VALUES(@orderId, @productId, @quantity, @price)')
+
+        for (let i = 0; i < productList.length; i++) {
+            let product = productList[i]
+            // If there is not a product or it is already in the database, we continue
+            if (!product || products.includes(product.id)) {
+                continue
+            }
+
+            // otherwise, add it
+            await insertInCart.execute({
+                orderId: orderId,
+                productId: product.id,
+                quantity: product.quantity,
+                price: product.price
+            })
+        }
+
+    } catch (err) {
+        console.dir(err)
+        return false;
+    } finally {
+        pool.close()
+    }
 }
 
 module.exports = router
