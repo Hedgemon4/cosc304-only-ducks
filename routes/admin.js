400e9955ce8d2943fd356b18288bedee0ccfae45
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2022-12-06 23:59:11 -0800

    able to change the product inventory

--- routes/admin.js
+++ routes/admin.js
@@ -12,10 +12,39 @@ router.get('/', function (req, res, next) {
                 let sqlQuery = "SELECT CAST(orderDate AS DATE) AS date, SUM(totalAmount) AS dailyTotal FROM ordersummary GROUP BY CAST(orderDate AS DATE)"
                 let results = await pool.request().query(sqlQuery)
                 let sales = results.recordset
-                let itemByWarehouse="Select product.productId as productid, product.productName as productname, p.warehouseId as wId, warehouse.warehouseName as wName, SUM(p.quantity) as quantity FROM productInventory p INNER JOIN product  on product.productId = p.productId INNER JOIN warehouse ON p.warehouseId=warehouse.warehouseId GROUP BY product.productId, product.productName, p.warehouseId,warehouse.warehouseName;"
+                let itemByWarehouse="SELECT product.productId AS productid, product.productName AS productname, p.warehouseId AS wId, warehouse.warehouseName AS wName, SUM(p.quantity) AS quantity FROM productInventory p INNER JOIN product  ON product.productId = p.productId INNER JOIN warehouse ON p.warehouseId=warehouse.warehouseId GROUP BY product.productId, product.productName, p.warehouseId,warehouse.warehouseName;"
                 let resultsWarehouse = await pool.request().query(itemByWarehouse);
                 let inventories=resultsWarehouse.recordset
                 res.render('admin', {dailySales: sales, inventoriesByWarehouse: inventories, title: "OnlyDucks Administrator Panel"})
+                console.log(req.query)
+                if(req.query.update && req.query.wId && req.query.newInventory){
+                    const ps = new sql.PreparedStatement(pool)
+                    let productId=req.query.update
+                    let wId=req.query.wId
+                    let newInventory=req.query.newInventory
+                    ps.input("productId", sql.Int)
+                    ps.input("wId", sql.Int)
+                    ps.input("newInventory", sql.Int)
+                    ps.prepare("UPDATE productInventory SET quantity=@newInventory WHERE productId=@productId AND warehouseId=@wId", (err) => {
+                        if (err) {
+                            console.error(err)
+                            return
+                        }
+                        ps.execute({productId: productId, wId: wId, newInventory: newInventory}, (err, result) => {
+                            if (err) {
+                                console.error(err)
+                                return
+                            }
+                            // console.log(result)
+                            ps.unprepare((err) => {
+                                if (err) {
+                                    console.error(err)
+                                }
+                            })
+                        })
+                    })
+
+                }
             } catch (err) {
                 console.dir(err)
                 res.end()
