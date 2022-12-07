f94646ea157457fc088dab8b8013ca82d46e8371
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2022-12-06 16:57:45 -0800

    the productquantity is being shown by warehouses

--- routes/admin.js
+++ routes/admin.js
@@ -12,17 +12,17 @@ router.get('/', function (req, res, next) {
                 let sqlQuery = "SELECT CAST(orderDate AS DATE) AS date, SUM(totalAmount) AS dailyTotal FROM ordersummary GROUP BY CAST(orderDate AS DATE)"
                 let results = await pool.request().query(sqlQuery)
                 let sales = results.recordset
-                let itemByWarehouse="Select product.productId as productidd, product.productName as productnamee, p.warehouseId as wIdd, warehouse.warehouseName as wNamee FROM productInventory p INNER JOIN product  on product.productId = p.productId INNER JOIN warehouse ON p.warehouseId=warehouse.warehouseId GROUP BY product.productId, product.productName, p.warehouseId,warehouse.warehouseName;"
+                let itemByWarehouse="Select product.productId as productid, product.productName as productname, p.warehouseId as wId, warehouse.warehouseName as wName, SUM(p.quantity) as quantity FROM productInventory p INNER JOIN product  on product.productId = p.productId INNER JOIN warehouse ON p.warehouseId=warehouse.warehouseId GROUP BY product.productId, product.productName, p.warehouseId,warehouse.warehouseName;"
                 let resultsWarehouse = await pool.request().query(itemByWarehouse);
                 let inventories=resultsWarehouse.recordset
-                res.render('admin', {dailySales: sales, title: "OnlyDucks Administrator Panel"})
-                res.render('admin',{inventoriesByWarehouse: inventories})
+                res.render('admin', {dailySales: sales, inventoriesByWarehouse: inventories, title: "OnlyDucks Administrator Panel"})
             } catch (err) {
                 console.dir(err)
                 res.end()
-            } finally {
+            }finally{
                 pool.close
             }
+
         })()
     }
 })
